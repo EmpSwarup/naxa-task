3ebb22fe5faf86c26f0d6a14e299071565db99ba
Swarup Sapkota <empswarupcoc@gmail.com>
2024-12-03 20:21:37 +0545

    Removed the `Navbar` component import from `App.jsx`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import './App.css'
 import NoticeBar from './components/NoticeBar/NoticeBar';
-import Navbar from './components/Navbar/Navbar';
 import LandingSection from './components/Landing/LandingSection';
 import ServicesSection from './components/Services/ServicesSection';
 
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <div>
       <NoticeBar />
-      <Navbar />
       <LandingSection />
       <ServicesSection />
     </div>
