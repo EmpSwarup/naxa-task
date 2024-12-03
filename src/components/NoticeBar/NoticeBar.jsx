ca047ea9973231e63e1b8088813aef886eab03ac
Swarup Sapkota <empswarupcoc@gmail.com>
2024-12-03 20:01:59 +0545

    Implement NoticeBar component

---
+++ src/components/NoticeBar/NoticeBar.jsx
@@ -0,0 +1,15 @@
+const NoticeBar = () => {
+  return (
+    <div className="flex justify-center items-center p-2 bg-[#ffdc1c] font-montserrat">
+      <a
+        href="https://www.naxa.com.np/blog/unified-action-naxa-nepal-flying-labs-and-partners-respond-to-the-western-nepal-earthquake-2023-28"
+        target="_blank"
+        className="text-black text-center text-sm underline hover:text-blue-600 font-semibold transition-all duration-300"
+      >
+        We Have Been Working On Several Initiatives During The Jajarkot-Rukum Earthquake Response 2023. Check Them Out
+      </a>
+    </div>
+  );
+};
+
+export default NoticeBar;
